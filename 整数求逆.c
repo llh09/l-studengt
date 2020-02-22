5e8dc51cb5a3d25417bee95449058a9da475d060
llh09 <61216123+llh09@users.noreply.github.com>
2020-02-22 09:17:42 +0800

    Create 整数求逆.c

---
+++ 整数求逆.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+   {
+   	int x;
+   	scanf("%d",&x);
+   	int digit;
+   	int result=0;
+   	while(x>0){
+   		digit=x%10;
+   		result=result*10+digit;
+   		x/=10;
+	   }
+	printf("%d",result);
+	return 0;
+   }
